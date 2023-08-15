ad9a62757489475184a3a0f31aeedceef3ec43a6
unknown <komauk1998@gmail.com>
2023-08-15 16:08:53 +0630

    main

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className='w-[15%] bg-black h-screen'>
+      <h1 className="text-white text-center">
+      <div className='flex items-center ms-7'>
+      </div>
+      </h1>
     </div>
   )
 }
