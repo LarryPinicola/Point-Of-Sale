523f43e37d08ce5082a4703c9a782da9d8f31563
unknown <komauk1998@gmail.com>
2023-08-17 14:19:54 +0630

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import React from 'react'
 import User from '../components/User'
 import LogOut from './LogOut'
 import Overview from './Overview'
@@ -18,13 +17,9 @@ const Dashboard = () => {
    </div>
    <div className="w-[85%] bg-[#202124]">
     <Overview/>
+   
    </div>
-//      <div className='w-[15%] bg-black h-screen'>
-//       <h1 className="text-white text-center">
-//       <div className='flex items-center ms-7'>
-//       </div>
-//       </h1>
-//     </div>
+
     </aside>
    </>
    
