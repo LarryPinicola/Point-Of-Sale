0f4a127cc9f8861f505e589fdfbbafd243943d53
LarryPinicola <thelarry1549@gmail.com>
2023-08-18 10:49:06 +0630

    edit

--- src/pages/EditFromMyAcc.jsx
+++ src/pages/EditFromMyAcc.jsx
@@ -1,3 +1,4 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import React from "react";
 import { AiOutlineEdit, AiOutlinePlus } from "react-icons/ai";
 import { BiSolidPhone } from "react-icons/bi";
@@ -26,7 +27,7 @@ const EditFromMyAcc = () => {
             </button>
           </div>
         </div>
-        <div className="bg-[#171717] p-16 mt-16 w-[84vw] h-[35vh] relative">
+        <div className="bg-[#171717] p-16 mt-16 w-[84vw] h-[20vh] relative">
           {/* profile pic */}
           <div className="">
             <div className="absolute top-[-50px]">
@@ -42,7 +43,7 @@ const EditFromMyAcc = () => {
             </div>
           </div>
           {/* name and icons */}
-          <div className="flex justify-between items-center relative top-[-70%]">
+          <div className="flex justify-between items-center relative top-[-50%]">
             <div className="ms-36">
               <h1 className="text-2xl tracking-wide">Ethan James</h1>
               <p className="text-gray-400 text-sm">
@@ -58,97 +59,171 @@ const EditFromMyAcc = () => {
               </div>
             </div>
           </div>
-          {/* sections */}
-          <div className="flex gap-8 mt-7">
-            {/* personal */}
-            <div className=" flex items-center gap-2 cursor-pointer">
-              <div className="text-blue-400">
-                <FaStore />
-              </div>
-              <h1 className="font-semibold text-lg tracking-wide text-gray-400">
-                Personal
-              </h1>
-            </div>
-            {/* Login info */}
-            <div className=" flex items-center gap-2 cursor-pointer">
-              <div className="text-blue-400">
-                <FaStore />
-              </div>
-              <h1 className="font-semibold text-lg tracking-wide text-gray-400">
-                Login Information
-              </h1>
-            </div>
-            {/* password */}
-            <div className="flex items-center gap-2 cursor-pointer">
-              <div className="text-blue-400">
-                <FaStore />
-              </div>
-              <h1 className="font-semibold text-lg tracking-wide text-gray-400">
-                Password
-              </h1>
-            </div>
-          </div>
         </div>
-        {/* form */}
-        <div className="border-t border-gray-500 bg-[#1A1A1A] w-[84vw] ">
-          <form action="" className="flex flex-col gap-5 mt-7 ms-16 ">
-            <div className="">
-              <label htmlFor="" className="me-[8rem]">
-                Name
-              </label>
-              <input
-                type="text"
-                className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
-                placeholder="Your Name"
-              />
-            </div>
-            <div className="">
-              <label htmlFor="" className="me-[7.8rem]">
-                Phone
-              </label>
-              <input
-                type="text"
-                className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
-                placeholder="Your Phone"
-              />
-            </div>
-            <div className="">
-              <label htmlFor="" className="me-[4.6rem]">
-                Date Of Birth
-              </label>
-              <input
-                type="text"
-                className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
-                placeholder="Your Date of birth"
-              />
-            </div>
-            <div className="flex items-center">
-              <label htmlFor="" className="me-[7rem]">
-                Gender
-              </label>
-              <div className="flex gap-5 items-center">
-                <input type="radio" className="outline-none" /> Male
-                <input type="radio" className="outline-none" /> Female
-              </div>
-            </div>
-            <div className="">
-              <label htmlFor="" className="me-[6.6rem]">
-                Address
-              </label>
-              <input
-                type="text"
-                className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
-              />
-            </div>
-          </form>
+        {/* sections TABs */}
+        <div className=" pt-5 bg-[#171717] ">
+          <Tabs variant={"line"}>
+            <TabList className="flex gap-10 items-center ms-16">
+              {/* personal tab */}
+              <Tab className="flex items-center gap-2 cursor-pointer">
+                <div className="text-blue-400">
+                  <FaStore />
+                </div>
+                <h1 className="font-semibold text-lg tracking-wide text-gray-400">
+                  Personal
+                </h1>
+              </Tab>
+              {/* login information */}
+              <Tab className="flex items-center gap-2 cursor-pointer">
+                <div className="text-blue-400">
+                  <FaStore />
+                </div>
+                <h1 className="font-semibold text-lg tracking-wide text-gray-400">
+                  Login Information
+                </h1>
+              </Tab>
+              {/* password tab */}
+              <Tab className="flex items-center gap-2 cursor-pointer">
+                <div className="text-blue-400">
+                  <FaStore />
+                </div>
+                <h1 className="font-semibold text-lg tracking-wide text-gray-400">
+                  Password
+                </h1>
+              </Tab>
+            </TabList>
+
+            <TabPanels className="mt-10 border-t border-gray-500 ">
+              {/* form */}
+              {/* personal */}
+              <TabPanel>
+                <div className="border-t border-gray-500 bg-[#1A1A1A] w-[84vw] py-5">
+                  <form action="" className="flex flex-col gap-10 mt-7 ms-16 ">
+                    <div className="">
+                      <label htmlFor="" className="me-[8rem]">
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[7.8rem]">
+                        Phone
+                      </label>
+                      <input
+                        type="text"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Phone"
+                      />
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[4.6rem]">
+                        Date Of Birth
+                      </label>
+                      <input
+                        type="text"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Date of birth"
+                      />
+                    </div>
+                    <div className="flex items-center">
+                      <label htmlFor="" className="me-[7rem]">
+                        Gender
+                      </label>
+                      <div className="flex gap-5 items-center">
+                        <input type="radio" className="outline-none" /> Male
+                        <input type="radio" className="outline-none" /> Female
+                      </div>
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[6.6rem]">
+                        Address
+                      </label>
+                      <input
+                        type="text"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </TabPanel>
+              {/* login information */}
+              <TabPanel>
+                <div className="border-t border-gray-500 bg-[#1A1A1A] w-[84vw] py-5">
+                  <form action="" className="flex flex-col gap-10 mt-7 ms-16 ">
+                    <div className="">
+                      <label htmlFor="" className="me-[7.8rem]">
+                        Phone
+                      </label>
+                      <input
+                        type="number"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Phone"
+                      />
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[8rem]">
+                        Email
+                      </label>
+                      <input
+                        type="text"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </TabPanel>
+              {/* password */}
+              <TabPanel>
+                <div className="border-t border-gray-500 bg-[#1A1A1A] w-[84vw] py-5">
+                  <form action="" className="flex flex-col gap-10 mt-7 ms-16 ">
+                    <div className="">
+                      <label htmlFor="" className="me-[7.8rem]">
+                        Current Password
+                      </label>
+                      <input
+                        type="password"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Phone"
+                      />
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[9.5rem]">
+                        New Password
+                      </label>
+                      <input
+                        type="password"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                    <div className="">
+                      <label htmlFor="" className="me-[7.6rem]">
+                        Confirm Password
+                      </label>
+                      <input
+                        type="password"
+                        className="p-2 text-sm rounded-lg bg-gray-600 outline-none"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                  </form>
+                </div>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </div>
         {/* footer */}
-        <div className="w-full h-16 border-t border-gray-500 bg-[#171717]">
-          <div className="mt-5 ms-10">
-            <button className="text-gray-600 border-gray-600 border py-1 text-sm px-3 rounded-lg uppercase me-5">
+        <div className="border-t border-gray-600 p-3 bg-[#171717] w-[84vw]">
+          <div className="flex gap-5 justify-end ">
+            <button className="rounded-lg uppercase tracking-wide border border-white py-1 px-3">
               cancel
             </button>
-            <button className="bg-blue-600 py-1 text-sm px-5 rounded-lg uppercase">
+            <button className="rounded-lg uppercase tracking-wide bg-blue-400 text-black py-1 px-3">
               save
             </button>
           </div>
